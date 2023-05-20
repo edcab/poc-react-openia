97787980094793d36a579d276af2b9db7a9eb797
EdwinCab <ecabezas.contractor@luminatedata.com>
2023-05-20 15:19:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// Create a react component that inputs a textarea message then performs a fetch request to localhost:3000 gets back a response as a data.message and displays that message on a text below
+
+import React, { Component } from 'react';
 
 function App() {
+  const [message, setMessage] = React.useState('');
+  const [response, setResponse] = React.useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:3002', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message }),
+    })
+
+    .then((res) => res.json())
+    .then((data) => setResponse(data.message))
+    .catch((err) => console.log(err));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{response}</p>
     </div>
   );
-}
+  }
 
 export default App;
